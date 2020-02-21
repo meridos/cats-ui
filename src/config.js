2fe944cc421192b39da11e18ffe238b5a0064cfd
Artem Bobrov <meriddos@gmail.com>
2020-02-21 13:31:32 +0300

    Изменены ссылки на API

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ const local = {
   reactionApi: 'http://localhost:3001',
 };
 const production = {
-  catsApi: 'http://cats.bobrovartem.ru/api',
-  reactionApi: 'http://cats.bobrovartem.ru/api/likes',
+  catsApi: 'https://cats.bobrovartem.ru/api',
+  reactionApi: 'https://cats.bobrovartem.ru/api/likes',
 };
 
 export const urls = process.env.NODE_ENV === 'production' ? production : local;
