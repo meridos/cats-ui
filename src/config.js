277979d287a693d903b6d45bc093d2b51ae2f077
Artem Bobrov <meriddos@gmail.com>
2020-02-26 10:42:57 +0300

    Обновление ссылок на апи

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 const local = {
-  catsApi: 'http://localhost:3001',
-  reactionApi: 'http://localhost:3001',
+  catsApi: 'http://localhost:6003',
+  reactionApi: 'http://localhost:6002',
+  photosApi: 'http://localhost:6001',
 };
 const production = {
-  catsApi: '/api',
+  catsApi: '/api/core',
   reactionApi: '/api/likes',
+  photosApi: '/api/photos',
 };
 
 export const urls = process.env.NODE_ENV === 'production' ? production : local;
