0eb423013a5b7e556c16f849ddbbebffc8ffd94f
Artem Bobrov <meriddos@gmail.com>
2020-03-20 08:04:04 +0300

    Версии приложения в одом месте
    Fix: не находил страницу
    
    Revert: ci manual
    
    Все версии в одном месте

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 fs.writeFile(
   './build/version.json',
   JSON.stringify({
-    ui: process.env.CI_COMMIT_REF_NAME || null,
+    version: process.env.CI_COMMIT_REF_NAME || null,
   }),
   () => {}
 );
